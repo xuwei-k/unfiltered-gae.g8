a9594f2826aff69df6048685627ecde479e2bdc8
xuwei-k <6b656e6a69@gmail.com>
2017-10-22 13:41:29 +0900

    add sbt 1.0 test

--- travis.sh
+++ travis.sh
@@ -9,4 +9,4 @@ unzip ${SDK_ZIP}
 unset APPENGINE_SDK_HOME
 export APPENGINE_SDK_HOME=$(pwd)/$SDK
 
-sbt ";set g8Properties in g8 in Test ~= { _ + (\"scala_version\" -> \"$TRAVIS_SCALA_VERSION\")}; g8Test"
+sbt ";set g8Properties in g8 in Test ~= { _ ++ Map((\"scala_version\" -> \"$TRAVIS_SCALA_VERSION\"), (\"sbt_version\" -> \"$SBT_VERSION\")) }; g8Test"
